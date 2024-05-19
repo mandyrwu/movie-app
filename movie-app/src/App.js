b30c71abd4f86b30afbea8670e4cdd58b588ca8c
mandyrwu <mandyrwu1@gmail.com>
2024-05-19 16:29:16 -0700

    set up the movie repository

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -7,7 +7,10 @@ import MovieList from './components/MovieList';
 function App() {
   return (
     <div className="App">
-      <h1>Popular Movies</h1>
+      <header className="App-header">
+        <h1>Popular Movies</h1>
+        <button className="add-review-button">Add Review</button>
+      </header>
       <MovieList />
     </div>
   );
--- movie-app/src/components/MovieList.js
+++ movie-app/src/components/MovieList.js
@@ -1,4 +1,5 @@
 // src/components/MovieList.js
+
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
@@ -19,17 +20,14 @@ const MovieList = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Popular Movies</h1>
-      <ul>
-        {movies.map(movie => (
-          <li key={movie.id}>
-            <h2>{movie.title}</h2>
-            <p>{movie.overview}</p>
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-          </li>
-        ))}
-      </ul>
+    <div className="movie-grid"> { }
+      {movies.map(movie => (
+        <div className="movie-item" key={movie.id}> { }
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+          <h2>{movie.title}</h2>
+          <p>{movie.overview}</p>
+        </div>
+      ))}
     </div>
   );
 };
